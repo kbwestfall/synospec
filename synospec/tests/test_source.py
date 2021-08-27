62025c21d8e86ae76bc3c066fbf97b053483b8b4
Kyle Westfall <westfall@ucolick.org>
2021-08-27 10:38:12 -0700

    test fix

--- synospec/tests/test_source.py
+++ synospec/tests/test_source.py
@@ -1,18 +1,38 @@
 
 from IPython import embed
 
+import numpy
+
 from synospec.etc.source import OnSkyMoffat, OnSkyGaussian
 
 def test_moffat():
 
-    gau = OnSkyGaussian(0.7, sampling=0.05, size=5.)
+    fwhm = 0.7
+    beta = 3.5
+    sampling = 0.05
+    size = 5.
 
-    mof = OnSkyMoffat(0.7, 3.5, sampling=0.05, size=5.)
+    gau = OnSkyGaussian(fwhm, sampling=sampling, size=size)
+    mean_gau_x = numpy.sum(gau.data*gau.X)/numpy.sum(gau.data)
+    mean_gau_y = numpy.sum(gau.data*gau.Y)/numpy.sum(gau.data)
+    assert numpy.allclose([mean_gau_x, mean_gau_y], [0,0]), 'Gaussian not centered'
 
-    from matplotlib import pyplot
+    peak = numpy.amax(gau.data)
+    indx = (gau.data > 0.47*peak) & (gau.data < 0.53*peak)
+    gau_fwhm = numpy.mean(2*numpy.sqrt(gau.X[indx]**2 + gau.Y[indx]**2))
+    assert numpy.absolute(fwhm - gau_fwhm) < sampling, 'Bad FWHM'
 
-    embed()
-    exit()
+    mof = OnSkyMoffat(fwhm, beta, sampling=sampling, size=size)
+    mean_mof_x = numpy.sum(mof.data*mof.X)/numpy.sum(mof.data)
+    mean_mof_y = numpy.sum(mof.data*mof.Y)/numpy.sum(mof.data)
+    assert numpy.allclose([mean_mof_x, mean_mof_y], [0,0]), 'Moffat not centered'
 
-if __name__ == '__main__':
-    test_moffat()
+    peak = numpy.amax(mof.data)
+    indx = (mof.data > 0.47*peak) & (mof.data < 0.53*peak)
+    mof_fwhm = numpy.mean(2*numpy.sqrt(mof.X[indx]**2 + mof.Y[indx]**2))
+    assert numpy.absolute(fwhm - mof_fwhm) < sampling, 'Bad FWHM'
+
+    assert numpy.array_equal(gau.x, mof.x), 'Coordinate arrays are different'
+    assert numpy.isclose(gau_fwhm, mof_fwhm), 'Gaussian and Moffat FWHM too different'
+
+
